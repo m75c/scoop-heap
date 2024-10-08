b080abe1ca23741935c7d9496a5ffad387bcfb17
m75c <github-action@actions-template-sync.noreply.github.com>
2024-10-08 21:11:26 +0000

    chore(template): merge template changes :up:
    Signed-off-by: m75c <github-action@actions-template-sync.noreply.github.com>

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "m75c/scoop-heap:main"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
